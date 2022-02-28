d48f0028ef6f02ff6c26b3af834735e0aa1433eb
Yurik <yurik522@gmail.com>
2022-02-28 20:59:03 +0300

    задача 9

---
+++ Example009/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 9. Показать последнюю цифру трехзначного числа
+
+//int fnumber = new Random().Next(100, 1000);
+//Console.Writline($"Последняя цифра {fnumber}");
+//if (fnumber > 99 & fnumber < 1000) {
+//    int a = fnumber % 10;
+//    Console.Write("Последняя цифра "
+
+using System;
+string numer = Convert.ToString(new Random().Next(100,1000));
+Console.WriteLine(numer);
+Console.WriteLine(numer.Substring(2));
