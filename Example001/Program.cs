3747e810077cd1d90011716670a397ccadb59032
Yurik <yurik522@gmail.com>
2022-02-28 19:59:06 +0300

    first commit out local

---
+++ Example001/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 1. Вывести квадрат числа
+Console.WriteLine("Введите число");
+// Конвертируем строку в целое число
+int numberValue = Convert.ToInt32(Console.ReadLine());
+int resultValue = numberValue*numberValue;
+Console.WriteLine(resultValue);
