2ab6cd133792c66fe30e0b34810b20b7aac178d5
Yurik <yurik522@gmail.com>
2022-03-11 12:58:50 +0300

    Изменил наименование папок

---
+++ Example011/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 11. Дано число из 
+// отрезка [10, 99]. Показать наибольшую цифру числа
+int numberA = new Random().Next(10, 99);
+
+Console.WriteLine($"Последнняя цифра числа {numberA}");
+{
+    int a = numberA % 10;
+    Console.Write($"равна {a}");
+}
