18f7a7a5216937f6b43fbefaeef3c9622232c812
Yurik <yurik522@gmail.com>
2022-03-13 13:02:54 +0300

    ДЗ семинар 1. Задача 2

---
+++ Homework/Seminars 1/Задача 2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+Console.WriteLine("ВВедите два числа");
+int numberA = Console.ReadLine();
+int numberB = Console.ReadLine();
+if (numberA > numberB)
+{
+    Console.Write("max=");
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.Write("max=");
+    Console.WriteLine(numberB);
+}
