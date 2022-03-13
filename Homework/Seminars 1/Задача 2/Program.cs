9fcfc3cfc1d00e3610d519e4f445f61396f481ee
Yurik <yurik522@gmail.com>
2022-03-13 15:00:45 +0300

    ДЗ семинар 1. Задача 2 с изменениями

--- Homework/Seminars 1/Задача 2/Program.cs
+++ Homework/Seminars 1/Задача 2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Написать программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
-Console.WriteLine("ВВедите два числа");
-int numberA = Console.ReadLine();
-int numberB = Console.ReadLine();
+Console.WriteLine("ВВедите два числа, между числами используйте 'Enter'");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
 if (numberA > numberB)
 {
     Console.Write("max=");
