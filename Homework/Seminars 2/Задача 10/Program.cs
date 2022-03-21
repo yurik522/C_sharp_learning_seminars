f3dc5044aab04e8c803308e59cf7b3ac3e1535ab
Yurik <yurik522@gmail.com>
2022-03-21 17:11:03 +0300

    ДЗ семинар 2. Задача 13

--- Homework/Seminars 2/Задача 10/Program.cs
+++ Homework/Seminars 2/Задача 10/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Написать программу, которая на входе принимает 3-х значное число, а на выходе показывает 
 //2-ю цифру этого числа.
 Console.WriteLine("Введите трехзрачное число");
-int numberA = Convert.ToInt32(Console.ReadLine());
+int numberA = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 int numberB = numberA % 100;
 if (numberB < 10)
 {
---
+++ Homework/Seminars 2/Задача 13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.WriteLine( "Введите первое число" );
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine( "ВВедите второе число" );
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int divNumber = firstNumber % secondNumber;
+if(divNumber == 0)
+    {
+        Console.Write($" {secondNumber} кратно  {firstNumber}");
+    }
+else
+{
+    Console.WriteLine($"{secondNumber} не кратно {firstNumber}");
+    Console.WriteLine($"Остаток {divNumber}");
+}
