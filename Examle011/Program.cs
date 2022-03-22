3c93b1f32a28fd7a29b8c521d1c75cd265b52a50
Yurik <yurik522@gmail.com>
2022-03-22 20:22:52 +0300

    ДЗ семинар 3. Задача 23

--- Examle011/Program.cs
+++ Examle011/Program.cs
@@ -1,3 +1,27 @@
-﻿string numer = Convert.ToString(new Random().Next(1000,10000));
-Console.WriteLine(numer);
-Console.WriteLine(numer.Substring(1));
+﻿//Показать таблицу квадратов чисел от 1 до N
+Console.WriteLine("введите число");
+
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"квадрат числа {i} =" + Math.Pow(i, 3));
+}
+// using System;
+
+// namespace My3
+// {
+//      class Program
+//      {
+//           static void Main(string[] args)
+//           {
+//                double x = 0.5;
+//                int stepen = 4;
+//                Console.WriteLine("0.5^4 = " + Math.Pow(x, stepen));
+//                Console.WriteLine("5^4 = " + Math.Pow(5, 4));
+//                Console.WriteLine("-5^4 = " + Math.Pow(-5, 4));
+//                Console.WriteLine("-5^(-4) = " + Math.Pow(-5, -4));
+//                Console.ReadKey();
+//           }
+//      }
+// }
---
+++ Homework/Seminars 3/Задача 23/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N
+Console.WriteLine("введите число");
+
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"куб числа {i} =" + Math.Pow(i, 3));
+}
