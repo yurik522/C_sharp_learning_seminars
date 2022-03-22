c5e02d7b8215b70f0f27446aab0560ab279e277a
Yurik <yurik522@gmail.com>
2022-03-22 20:53:08 +0300

    ДЗ семинар 3. Задача 21

---
+++ Homework/Seminars 3/Задача 21/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая на вход принимает координаты точек, а на 
+// выходе выдает расстояние между ними в 3D пространстве
+Console.WriteLine("Координаты первой точки");
+
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Координаты второй точки");
+
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+double S = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Расстояние между точками = {S}");
