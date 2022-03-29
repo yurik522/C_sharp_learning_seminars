db116bc97b24b5351540cf5072b78306a803cec0
Yurik <yurik522@gmail.com>
2022-03-29 18:02:46 +0300

    дз семинар 4. адача 25

---
+++ Homework/Seminar 4/Задача 25/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("введите число A");
+
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число B");
+
+int B = Convert.ToInt32(Console.ReadLine());
+// double result = Math.Pow(A, B);
+// Console.WriteLine(result);
+
+int BExp = 1;
+
+for (int i = 1; i <=B; i++)
+{
+    BExp = BExp * A;
+}
+Console.WriteLine($"Число {A}, возведенное в степень {B} равно {BExp}");
