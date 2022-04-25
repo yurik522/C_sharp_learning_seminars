6fb9bbe923a15596f8e496fff427e69a881f1d4c
Yurik <yurik522@gmail.com>
2022-04-25 11:29:47 +0300

    Homework_Seminar8

---
+++ Homework/Seminar 8/Задача 54/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по 
+//убыванию элементы каждой строки двумерного массива(в пределах всего массива).
+void FillArray(int[,] newArray)
+{
+    Random rnd = new Random();
+    for(int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < newArray.GetLength(1); j++)
+        {
+            newArray[i, j] = rnd.Next(-20, 20);
+        }
+
+    }
+}
+void PrintArray(int[,] newArray)
+{
+    for(int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < newArray.GetLength(1); j++)
+        {
+            Console.Write("{0,4}", newArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+void SelectionSort(int[,] SortArray) 
+{
+   for(int i = 0; i < SortArray.GetLength(0); i ++)
+    {
+        for(int j = 0; j < SortArray.GetLength(1) - 1; j ++)
+        {          
+            int maxPosition = j; 
+            for(int k = j + 1; k < SortArray.GetLength(1); k ++)
+            {
+               if(SortArray[i, k] > SortArray[i, maxPosition]) maxPosition = k;
+            }
+            int temporary = SortArray[i, j];
+            SortArray[i, j] = SortArray[i, maxPosition];
+            SortArray[i, maxPosition] = temporary;                            
+            
+        }
+    }
+}
+
+// {
+//     for(int i = 0; i < array.Length - 1; i++)
+//     {
+//          int maxPosition = i; 
+//         for(int j = i + 1; j < array.Length; j++ )
+//         {
+//             if(array[j] > array[maxPosition]) maxPosition = j;
+//         }
+//         int temporary = array[i];
+//         array[i] = array[maxPosition];
+//         array[maxPosition] = temporary;                            
+//     }
+// }
+Console.WriteLine("Введите количество строк в массиве:");
+int s = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве:");
+int c = Convert.ToInt16(Console.ReadLine());
+int[,] array = new int[s,c];
+FillArray(array);
+PrintArray(array);
+//AverageColumns(array);
+//PrintArray(array);
+Console.WriteLine();
+SelectionSort(array);
+PrintArray(array);
---
+++ Homework/Seminar 8/Задача 56/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+void FillArray(int[,] newArray) //заполяем массив
+{
+    Random rnd = new Random();
+    for(int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < newArray.GetLength(1); j++)
+        {
+            newArray[i, j] = rnd.Next(-15, 15);
+        }
+
+    }
+}
+void PrintArray(int[,] newArray)//печатаем массив
+{
+    for(int i = 0; i < newArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < newArray.GetLength(1); j++)
+        {
+            Console.Write("{0,4}", newArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+void SumString(int[,] newArray) //печатаем суммы элементов в каждой строке   
+{
+    int sumString = 0;
+    for(int i = 0; i < newArray.GetLength(0); i ++)
+    {
+        for(int j = 0; j < newArray.GetLength(1); j ++)
+        {
+            sumString = sumString + newArray[i, j];
+        }
+        //double average = sumColum/(newArray.GetLength(0));
+        Console.Write($"Сумма элементов {i + 1}-й строки равна ");
+        Console.Write(sumString);
+        sumString = 0;
+        Console.WriteLine();
+    }
+}
+int[] ArraySumString(int[,] newArray) //собираем минимальные значения сумм чисел в строках в массив   
+{
+    int sumString = 0;
+    int[] ArraySum = new int[newArray.GetLength(0)];
+    for(int i = 0; i < newArray.GetLength(0); i ++)
+    {
+        for(int j = 0; j < newArray.GetLength(1); j ++)
+        {
+            sumString = sumString + newArray[i, j];
+        }
+        ArraySum[i] = sumString;
+        sumString = 0;
+    }
+    return ArraySum;
+}
+void minSumString(int[] arraySort) //находим минимальное значение и выдаём результат
+{
+    int i = 0;
+    int min = arraySort[i];
+    int minIndex = i;
+    for (i = 0; i < arraySort.Length; i++)
+    {
+        if(arraySort[i] < min)
+        { 
+          min = arraySort[i];
+          minIndex = i + 1; //фиксируем строку, в которой находится минимальная сумма элеметов. Добавляем к значению
+                            //единицу, чтобы вывод был логически понятным рядовому пользователю.
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Минимальная сумма элементов находится в {minIndex}-й строке  и равна {min}"); 
+}
+Console.WriteLine("Введите количество строк в массиве:");
+int s = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в массиве:");
+int c = Convert.ToInt16(Console.ReadLine());
+int[,] array = new int[s,c];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+SumString(array);
+minSumString(ArraySumString(array));
